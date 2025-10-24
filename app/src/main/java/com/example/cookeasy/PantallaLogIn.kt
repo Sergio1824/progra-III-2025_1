ffddbd48ef32645cae05a485414b500fe24eaf1f
Joel <joelduraznoflores@gmail.com>
2025-10-23 22:12:18 -0400

    Implementacion de la pantalla de Login, que te dirige a MainActivity

--- app/src/main/java/com/example/cookeasy/PantallaLogIn.kt
+++ app/src/main/java/com/example/cookeasy/PantallaLogIn.kt
@@ -1,23 +1,24 @@
 package com.example.cookeasy
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.cookeasy.databinding.ActivityMainBinding
 import com.example.cookeasy.databinding.ActivityPantallaLogInBinding
 
-class MainActivity : AppCompatActivity() {
+class PantallaLogIn : AppCompatActivity() {
     val context: Context = this
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityPantallaLogInBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityPantallaLogInBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
@@ -26,5 +27,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //TO_DO LO QUE ESTA ACA ESTA DENTRO DEL ONCREATE
+
+        binding.ButtonLogin.setOnClickListener {
+            val intentCambiarAMainActivity= Intent(context, MainActivity::class.java)
+            startActivity(intentCambiarAMainActivity)
+        }
+
+
+
     }
 }
