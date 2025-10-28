1fc9a1fd1cfee156691972be8c7cc9c386d8cca6
Joel <joelduraznoflores@gmail.com>
2025-10-27 20:00:09 -0400

    IMPLEMENTACION DEL RECYCLERVIEW, JUNTO AL ADAPTER Y ADICION DE UNA CLASE EXTRA DE XML PARA LA TARJETA QUE SE USARE EN RECYCLER VIEW

--- app/src/main/java/com/example/cookeasy/adapters/AdapterPantallaCategoria.kt
+++ app/src/main/java/com/example/cookeasy/adapters/AdapterPantallaCategoria.kt
@@ -4,20 +4,21 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.cookeasy.databinding.ActivityPantallaCategoriasBinding
+import com.example.cookeasy.dataClasses.Categoria
+import com.example.cookeasy.databinding.VistasCategoriasBinding
 
 
 class AdapterPantallaCategoria :
     RecyclerView.Adapter<AdapterPantallaCategoria.EjemploCardViewHolder>() {
 
-    private val dataCards = mutableListOf<String>()
+    private val dataCards = mutableListOf<Categoria>()
     private var context: Context? = null
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EjemploCardViewHolder {
         context = parent.context
         return EjemploCardViewHolder(
-            ActivityPantallaCategoriasBinding.inflate(
+            VistasCategoriasBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
                 false
@@ -32,9 +33,9 @@ class AdapterPantallaCategoria :
     override fun getItemCount(): Int = dataCards.size
 
     // Donde hacer la logica
-    inner class EjemploCardViewHolder(private val binding: ActivityPantallaCategoriasBinding) :
+    inner class EjemploCardViewHolder(private val binding: VistasCategoriasBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun binding(data: String) {
+        fun binding(data: Categoria) {
       //      binding.textAdapterEjemplo.text = data
 
 
@@ -42,7 +43,7 @@ class AdapterPantallaCategoria :
         }
     }
 
-    fun addDataCards(list: List<String>) {
+    fun addDataCards(list: List<Categoria>) {
         dataCards.clear()
         dataCards.addAll(list)
     }
